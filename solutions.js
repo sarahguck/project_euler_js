177d2472127b0a41d3b8a5160b86fc13536a64d5
Sarah Guck <sarah@graybits.biz>
2015-02-02 09:02:04 -0600

    Add solution 6.

--- solutions.js
+++ solutions.js
@@ -5,7 +5,7 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
-function solve1(){
+function solve1() {
   var sum = 0;
   for (var i = 999; i >= 0; i--) {
     if (i % 5 == 0 || i % 3 == 0) {
@@ -23,11 +23,11 @@ function solve1(){
 // 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 // By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
-function solve2(){
+function solve2() {
   var fib = [1, 2];
   var sum = 2;
 
-  function getNext() {
+  function getNext()  {
     next = fib[0] + fib[1];
     fib.shift();
     fib.push(next);
@@ -51,7 +51,7 @@ function solve2(){
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143 ?
 
-function solve3(){
+function solve3() {
   var num = 600851475143;
   var res = 0;
   var guess = 0;
@@ -84,7 +84,7 @@ function isPrime(n) {
 // A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
-function solve4(){  
+function solve4() {   
   var max = 999*999;
   var min = 100*100;
   var prod = 0;
@@ -116,7 +116,7 @@ function solve4(){
 // x <= ceil/2 is out since it will be represented in (ceil/2..ceil]
 // ie any multiple of ceil is a multiple of ceil/2
 
-function solve5(){  
+function solve5() {   
   var ceil = 20;
   var floor = ceil/2;
   var factors = {};
@@ -159,7 +159,7 @@ function getPrimeFactors(n) {
         factor_arr.push(i);
       }
     }
-    var prod = factor_arr.reduce(function(a,b){ return a*b; });
+    var prod = factor_arr.reduce(function(a,b){ return a*b; }) ;
     if (prod == n) {
       return factor_arr;
     } else {
@@ -170,7 +170,28 @@ function getPrimeFactors(n) {
 }
 
 function flatten(arr) {
-  return arr.reduce(function (flat, toFlatten) {
+  return arr.reduce(function (flat, toFlatten)  {
     return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
   }, []);
+}
+
+////////////////////////////////////////////////////////////////////////////
+// Problem 6 ///////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+// The sum of the squares of the first ten natural numbers is,
+// 12 + 22 + ... + 102 = 385
+// The square of the sum of the first ten natural numbers is,
+// (1 + 2 + ... + 10)2 = 552 = 3025
+// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+function solve6() {   
+  var n = 100;
+  var sum_of_squares = 0;
+  var square_of_sum  = Math.pow(n * (n + 1) / 2, 2);
+  for (var i = n; i > 0; i--) {
+    sum_of_squares += Math.pow(i, 2);
+  };
+  return Math.abs(square_of_sum - sum_of_squares);
 }
